a78860624e1de0eea995bce6bc4b1d34f920b574
bishwajit1965 <paul.bishwajit09@gmail.com>
2019-01-17 23:19:06 +0600

    "Committed"

--- student/add_student.php
+++ student/add_student.php
@@ -15,42 +15,6 @@
                             require_once '../App/start.php';
                             // Classes included
 
-                            // Trait instantiated
-                            $objectTrait = new MyHelloWorld();
-                            $objectTrait->sayHello().'<br>';
-
-                            // Design pattern instantiated
-                            $paperback = new PaperBack();
-                            $paperback->setTitle('A Book on Wild Animals of Africa');
-                            $paperback->setContent('Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Amet sequi ut, earum, distinctio, dignissimos totam dolores est saepe magnam obcaecati qui.
-                            Expedita accusamus, repudiandae magnam corrupti officia facilis at officiis?');
-                            echo $paperback->printBook();
-
-                            $ebook = new Ebook();
-                            $ebook->setTitle('A Book on Wild Animals of Africa');
-                            echo $ebook->generatePdf();
-
-                            // Factory instantiated
-                            echo '<h2>Factory design pattern</h2>';
-
-                            $automobile = AutomobileFactory::create('Bugatti', 'Veyron');
-                            echo $automobile->getMakeAndModel();
-
-                            // Inheritance
-                            echo '<h2>Inheritance</h2>';
-                            use Storage\Inheritance\Book as Book;
-
-                            $book = new Book();
-                            $book->store();
-                            $book->index();
-                            // Tester
-
-                            // StrategyPattern
-                            $email = new SendEmail();
-                            $email->message();
-                            $message = SendMsg;
-                            $message->message();
                             ?>
                             <!-- ***** Write code above *****-->
                         </div>
